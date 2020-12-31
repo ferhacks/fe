bfe42dabd99592361efdd589d679b17fbd60fe6e
ferhacks <63879582+ferhacks@users.noreply.github.com>
2021-01-01 00:52:39 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-if [ -n "${BASE_CONF}" ] && [ -n "${CLOUD_NAME}" ] ; then
+if [ -n "I1RodSBEZWMgMzEgMjM6NDg6NTUgVVRDIDIwMjAKW2xvbF0KdHlwZSA9IGRyb3Bib3gKY2xpZW50X2lkID0gbDN0NGZzOGpoMTY4Nm56CmNsaWVudF9zZWNyZXQgPSBsZW1qcTN5dzg1bG05N20KdG9rZW4gPSB7ImFjY2Vzc190b2tlbiI6InNsLkFvY09GSUNWRDQ3REk3Ty1LVF9FblNSUHJVRlVKcktJdk42eDUwTExYTGFEOExIanIzNXZtSF82OFFaX3BiZThUMmtURzlRUFdlQWkzb1hYWW5WaUV3M3oxLVpXbGN4Qlp3MUxIaExwb3NpeUpta1FuVHVSRnNrdDY3ZlRCQjl5TUVvREFrMCIsInRva2VuX3R5cGUiOiJiZWFyZXIiLCJleHBpcnkiOiIyMDIxLTAxLTAxVDAzOjIzOjIwLjY1MTcxNTkyMVoifQo=" ] && [ -n "lol" ] ; then
     pgrep rclone
     if [ $? -eq 0 ]; then
         echo "already mounred skipping"
